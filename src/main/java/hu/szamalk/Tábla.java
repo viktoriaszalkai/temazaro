6ba3ae4a4c44145d8c54ab5332efd5f914f7de0e
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-12-09 13:36:28 +0100

    f1 3

--- src/main/java/hu/szamalk/Tábla.java
+++ src/main/java/hu/szamalk/Tábla.java
@@ -3,6 +3,16 @@ package hu.szamalk;
 public class Tábla {
     private Character ÜresCella;
     private Character [][] T;
-    
+    private int uresOszlopokSzama, uresSorokSzama;
+
+    public Tábla(Character üresCella, Character[][] t) {
+        this.ÜresCella = üresCella;
+        T = new Character[8][8];
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                T[i][j] = ÜresCella;
+            }
+        }
+    }
 
 }
