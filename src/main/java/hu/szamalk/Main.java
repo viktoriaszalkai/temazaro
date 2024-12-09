01f03b790e378379968fed64a693ae9b8fb7a7d5
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-12-09 13:58:41 +0100

    f1 6

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -2,10 +2,11 @@ package hu.szamalk;
 
 public class Main {
     public static void main(String[] args) {
-        Tábla t1 = new Tábla('*');
+        Tábla t1 = new Tábla('#');
+        System.out.println("4. feladat: Az üres tábla:");
         t1.megjelenit();
-        System.out.println("-----------");
-        t1.elhelyez(6);
+        System.out.println("6. feladat: Az feltöltött tábla:");
+        t1.elhelyez(8);
         t1.megjelenit();
     }
 }
